35e0512d0652ced046e3f1df2e03aeab7bb2cc37
Nikita <loskutov.2810@gmail.com>
2025-11-05 20:47:42 +0500

    CREATE refresh middleware

--- frontend-admin/src/services/api.ts
+++ frontend-admin/src/services/api.ts
@@ -1,19 +1,71 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { ILoginBody, ILoginResponseBody, ILoginConfigmBody, ILoginConfirmResponseBody } from "../dto/IAuth";
+import { Mutex } from "async-mutex";
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from "@reduxjs/toolkit/query";
+
+const mutex = new Mutex();
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: '/api/v1',
+    credentials: 'include',
+    prepareHeaders: (headers) => {
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            headers.set('Authorization', `Bearer ${token}`);
+        }
+        return headers;
+    },
+});
+
+const baseQueryWithReauth: BaseQueryFn<
+    string | FetchArgs,
+    unknown,
+    FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+    await mutex.waitForUnlock();
+
+    let result = await baseQuery(args, api, extraOptions);
+
+    if (result.error && result.error.status === 401) {
+        if(!mutex.isLocked()) {
+            const release = await mutex.acquire();
+
+            try {
+                const refreshResult = await baseQuery(
+                    {
+                        url: '/auth/refresh',
+                        method: 'POST',
+                    },
+                    api,
+                    extraOptions
+                );
+
+                if(refreshResult.data) {
+                    const newAccessToken = (refreshResult.data as ILoginConfirmResponseBody).accessToken;
+
+                    localStorage.setItem('accessToken', newAccessToken);
+
+                    result = await baseQuery(args, api, extraOptions);
+                } else {
+                    localStorage.removeItem('accessToken');
+                    window.location.href = '/login';
+                }
+            } finally {
+                release();
+            }
+        } else {
+            await mutex.waitForUnlock();
+
+            result = await baseQuery(args, api, extraOptions);
+        }
+    } 
+
+    return result;
+}
 
 export const api = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
-        baseUrl: '/api/v1',
-        credentials: 'include',
-        prepareHeaders: (headers) => {
-            const token = localStorage.getItem('accessToken');
-            if (token) {
-                headers.set('Authorization', `Bearer ${token}`);
-            }
-            return headers;
-        },
-    }),
+    baseQuery: baseQueryWithReauth,
 
     tagTypes: [],
 
