dac569d5d9201b6d2db25ac388e170a4362273e6
Nikita <loskutov.2810@gmail.com>
2025-12-01 21:09:16 +0500

    ADD file

--- frontend-admin/dev-dist/sw.js
+++ frontend-admin/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.piobq9cqvd"
+    "revision": "0.683ur5lap0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
